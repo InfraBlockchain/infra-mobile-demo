506bf2b3ebc206e38d68361e21f11222ef112b3c
zustkeeper <zustkeeper@naver.com>
2021-08-30 18:22:25 +0900

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,84 @@
+require('node-libs-expo/globals');
+import * as encoding from 'text-encoding';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import InfraDID from 'infra-did-js'
+import {decodeJWT} from 'did-jwt';
+import * as Random from 'expo-random';
+import * as Clipboard from 'expo-clipboard';
 
 export default function App() {
+
+  const [identity, setIdentity] = React.useState({});
+  const [jwt, setSignedJwt] = React.useState('');
+  const [decodedData, setDecodedData] = React.useState({});
+
+  // Set infra-did-js config
+  const confBlockchainNetwork = {
+    networkId: '01',
+    registryContract: 'infradidregi',
+    rpcEndpoint: 'https://api.testnet.infrablockchain.com',
+  }
+
+  const createSignedJWT = async () => {
+    // Create did (public & private key pair)
+    const identity = InfraDID.createPubKeyDIDsecp256k1('01');
+    setIdentity(identity);
+
+    // Set Signer config
+    const conf = {
+      ...confBlockchainNetwork,
+      did: `${identity.did}`,
+      didOwnerPrivateKey: `${identity.privateKey}`,
+    }
+    const didApi = new InfraDID(conf);
+
+    // Given challenge value
+    const challenge = Random.getRandomBytes(1)[0];
+
+    // Set payload of signedJWT
+    const payload = {
+      nonce: challenge,
+      payload: {
+        company: 'moca',
+      }
+    }
+
+    //Sign JWT
+    const jwt = await didApi.signJWT(payload);
+    const decodedJWT = decodeJWT(jwt).payload;
+    setSignedJwt(jwt);
+    setDecodedData(decodedJWT);
+    console.log('identity>\n', identity);
+    console.log('\n');
+    console.log('jwt>\n', jwt);
+    console.log('\n');
+    console.log('decoded>\n', decodedJWT);
+
+    Clipboard.setString(jwt);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Blockchain Labs</Text>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>DID</Text>
+        <Text style={styles.subtitle}>{identity.did && `did:\n${identity.did}`}</Text>
+        <Text style={styles.subtitle}>{identity.privateKey && `privateKey:\n${identity.privateKey}`}</Text>
+        <Text style={styles.subtitle}>{identity.publicKey && `publicKey:\n${identity.publicKey}`}</Text>
+        <Text style={styles.title}>SignedJWT</Text>
+        <Text style={styles.subtitle}>{jwt && `${jwt}`}</Text>
+        <Text style={styles.title}>DecodedJWT</Text>
+        <Text style={styles.subtitle}>{decodedData && `${JSON.stringify(decodedData)}`}</Text>
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.button} onPress={createSignedJWT}>
+          <Text style={styles.buttonText}>{`생성하기 & JWT 클립보드 복사`}</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,6 +87,38 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  contentContainer: {
+    width: Dimensions.get('window').width * .8,
+    // height: Dimensions.get('window').height * .7,
+    borderWidth: 1,
+    borderColor: 'black',
+    paddingHorizontal: 24,
+    paddingBottom: 20,
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  subtitle: {
+    fontSize: 12,
+  },
+  footer: {
+    height: 50,
+  },
+  button: {
+    width: Dimensions.get('window').width * .8,
+    height: 50,
+    backgroundColor: '#0036AF',
+    borderRadius: 12,
+    alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 });
---
+++ readme.md
@@ -0,0 +1,29 @@
+## Requirements
+- [Node.js](https://nodejs.org/en/download/) (includes npm)
+- [Expo-CLI](https://docs.expo.io/workflow/expo-cli/)
+- [Watchman](https://facebook.github.io/watchman/docs/install#buildinstall) for macOS users
+
+## Recommended Tools
+- [Yarn](https://classic.yarnpkg.com/en/docs/install)
+
+## Installation
+```bash
+# yarn (recommended)
+$ yarn
+
+# npm
+$ npm install
+
+```
+
+## Running the app
+```bash
+# run
+$ yarn start
+
+# open your app on Android emulator
+$ yarn android
+
+# open your app on iOS simulator
+$ yarn ios
+```
